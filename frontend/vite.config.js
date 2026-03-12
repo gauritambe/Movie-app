55026ca849229674f5f1faf7c23560a458e74016
Devops <devops@serverc.example.com>
2026-03-13 00:48:04 +0530

    Final changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:3000",
-      "/uploads/": "http://localhost:3000",
+      "/api/": "http://backend:3000",
+      "/uploads/": "http://backend:3000",
     },
     fs: {
       allow: [
